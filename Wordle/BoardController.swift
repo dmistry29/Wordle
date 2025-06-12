2e276fd40bb15545c7e4234b8edbd7d92280f696
dmistry29 <159323051+dmistry29@users.noreply.github.com>
2025-06-11 21:28:16 -0400

    Update BoardController.swift

--- Wordle/BoardController.swift
+++ Wordle/BoardController.swift
@@ -25,7 +25,6 @@ class BoardController: NSObject,
     collectionView.dataSource = self
   }
 
-  // MARK: - Public Methods
   func enter(_ string: String) {
     guard numGuesses < numItemsPerRow * numRows else { return }
     let cell = collectionView.cellForItem(at: IndexPath(item: numGuesses, section: 0)) as! LetterCell
@@ -34,12 +33,7 @@ class BoardController: NSObject,
                    delay: 0.0,
                    options: [.autoreverse],
                    animations: {
-      // Exercise 7: Change the scale of the cell by 1.05
-      // Tip: Use the transform property of the cell. Use transform.scaledBy to modify the scale. This should feel familiar to lab 1.
-      // Checkpoint: After finishing this exercise, you should now be able to see that the board animates whenever you enter a new letter! If it's not animating, check your work on this exercise.
-      // START YOUR CODE HERE
       cell.transform = cell.transform.scaledBy(x: 1.05, y: 1.05)
-      // END YOUR CODE HERE
     }, completion: { finished in
       cell.transform = CGAffineTransformIdentity
     })
@@ -53,11 +47,7 @@ class BoardController: NSObject,
     guard numGuesses > 0 && numGuesses % numItemsPerRow != 0 else { return }
     let cell = collectionView.cellForItem(at: IndexPath(item: numGuesses - 1, section: 0)) as! LetterCell
     numGuesses -= 1
-    // Exercise 6: Look at the available LetterCell's methods to clear the letter and set the style to `initial`
-    // Tip: Checkout the public methods on LetterCell.swift
-    // Checkpoint: After finishing this exercise, you should now be able to tap on the delete keyboard cell and have the last letter deleted on the board! If it's not working, check your work on this exercise and make sure deleteLastCharacter() is called properly in exercise 3.
-    // START YOUR CODE HERE
+
     cell.clearLetter()
-    // END YOUR CODE HERE
   }
 }
